e8ddd327820f05c599bba4bb2f663e553b8635a7
monkARC <monkarc59@gmail.com>
2018-10-17 16:44:37 +0530

    shortcut render learning

--- blog/views.py
+++ blog/views.py
@@ -9,8 +9,9 @@ from django.http import HttpResponse
 
 
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	return render(request, 'blog/home.html')
 
 
 def about(request):
 	return HttpResponse('<h1>Blog About Page</h1>')
+
