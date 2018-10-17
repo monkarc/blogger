8abe66180243fed0909bb9b04903694da10c7146
monkARC <monkarc59@gmail.com>
2018-10-17 17:21:06 +0530

    posts_context_jinja(templating_engine)

--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,27 @@ from django.http import HttpResponse
 # templates for our blog are in templates folder 
 # blog -> templates -> blog -> template.html
 
+posts=[
+{
+	'author':'John doe',
+	'title':'Blog Post 1',
+	'content':'First post contents',
+	'date_posted':'2018-10-17'
+},
+{
+	'author':'Jane doe',
+	'title':'Blog Post 2',
+	'content':'Second post contents',
+	'date_posted':'2018-10-18'
+}
 
+]
 
 def home(request):
-	return render(request, 'blog/home.html')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context) #context is now accesible in .html page
 
 
 def about(request):
