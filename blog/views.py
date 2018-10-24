da3fd5e3b00cbe533763c347c0e960db6b61940c
monkARC <monkarc59@gmail.com>
2018-10-24 12:59:30 +0530

    Class_based_views > ListView | template for views

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from django.http import HttpResponse
 from .models import Post   #because models is in same dir .model is used
-
 
 # Create your views here.
 
@@ -15,6 +15,24 @@ def home(request):
 	return render(request, 'blog/home.html', context) #context is now accesible in .html page
 
 
+# class based views look for certain template with naming convention
+# The naming convention is <app>/<model>_<viewtype>.html
+# when template with this name is unavailable, \
+#  we can then redirect to already available template using 'template_name'
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
 def about(request):
 	return render(request, 'blog/about.html', {'title': 'About'})
 
+
+# CLASS BASED VIEWS
+# 1. update views
+# 2. list views (blog, subscription on youtube)
+# 3. detail views
+# 4. create views
+# 5. delete views
--- users/views.py
+++ users/views.py
@@ -4,7 +4,6 @@ from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
-
 # Create your views here.
 def register(request):
     if request.method == 'POST':
