7e28b08f0c619dd7d47ab97a1a2a51a0b2aecf28
monkARC <monkarc59@gmail.com>
2018-10-24 14:37:21 +0530

    LoginRequiredMixin for restricting Anonymous user from access /post/new page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (ListView,
                                   DetailView,
                                   CreateView)
@@ -34,10 +35,9 @@ class PostDetailView(DetailView):
     model = Post
 
 
-
 # CreateView/UpdateView looks for certain template with naming convention
 # The naming convention is <app>/<model>_form.html
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title','content']
 
