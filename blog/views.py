fd3f81aac5c4ae039e387adf33c03e138054cdb7
monkARC <monkarc59@gmail.com>
2018-10-24 14:45:20 +0530

    UpdateView | LoginRequiredMixin

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (ListView,
                                   DetailView,
-                                  CreateView)
+                                  CreateView,
+                                  UpdateView)
 from django.http import HttpResponse
 from .models import Post   #because models is in same dir .model is used
 
@@ -38,6 +39,16 @@ class PostDetailView(DetailView):
 # CreateView/UpdateView looks for certain template with naming convention
 # The naming convention is <app>/<model>_form.html
 class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title','content']
+
+    # This function is used to tag the author to the New post being created.
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['title','content']
 
